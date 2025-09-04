aae60ce23c0a82ebe7aa3b120a82f34706abd46c
corvuscorae <corvuscorae@gmail.com>
2025-09-04 12:29:08 -0700

    tbd path gen

---
+++ src/3_Generators/generatePaths.js
@@ -0,0 +1,23 @@
+import WFCModel from "../2_WFC/1_Model/wfcModel.js";
+import IMAGES from "../2_WFC/2_Input/images.js";
+import TILEMAP from "../4_Phaser/TILEMAP.js";
+
+const model = new WFCModel().learn(IMAGES.GROUND, 2);
+
+/**
+ * @param {BoundingBox} boundingBox
+ * @returns {TilemapImage}
+ */
+export default function generatePaths(boundingBox) {
+  model.clearSetTiles();
+
+  // generate paths
+  const paths = model.generate(boundingBox.width, boundingBox.height, 10, false, false);
+
+  if (!paths){ 
+    console.error("Contradiction created");
+    return false;
+  }
+
+  return paths;
+}
